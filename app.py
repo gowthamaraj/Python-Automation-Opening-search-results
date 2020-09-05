a94ec170c44fc98c033d9113840be638e51214a9
gowthamaraj <gowthamaraj360@gmail.com>
2020-09-05 23:08:45 +0530

    version 1

---
+++ app.py
@@ -0,0 +1,26 @@
+import sys
+import requests
+import bs4
+import webbrowser
+import pprint
+import pyperclip as clipboard
+
+print('Searching...')
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
+url = 'https://www.google.com/search?q='
+
+if len(sys.argv) > 1:
+    keyword = ' '.join(sys.argv[1:])
+else:
+    keyword = clipboard.paste()
+    
+query = url + keyword
+res = requests.get(query, headers= headers)
+
+parsed = bs4.BeautifulSoup(res.text, 'html.parser')
+g_link = parsed.select('.r a')
+links_filtered = [link.get('href') for link in g_link if '#' not in str(link) if 'webcache' not in str(link)]
+
+for link in links_filtered[:5]:
+    webbrowser.open_new_tab(link)
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Python Automation
+
+>  simply type a search term on the command line and have my computer automatically open a browser with all
+the top search results in new tabs.
+
+## TODO - Steps
+1. Read the command line arguments from sys.argv.
+2. Fetch the search result page with the requests module.
+3. Find the links to each search result.
+4. Call the webbrowser.open() function to open the web browser.
+
